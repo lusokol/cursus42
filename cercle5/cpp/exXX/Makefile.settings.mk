3b979b0b9cbe2538ac7d76e1bf173cd26a32fcd1
LuluLink <sokol.lucas@gmail.com>
2022-02-07 16:30:51 +0100

    maj exos

--- cercle5/cpp/exXX/Makefile.settings.mk
+++ cercle5/cpp/exXX/Makefile.settings.mk
@@ -6,17 +6,15 @@
 #    By: lusokol <lusokol@student.42.fr>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/10/21 21:06:11 by rotrojan          #+#    #+#              #
-#    Updated: 2022/02/07 16:29:05 by lusokol          ###   ########.fr        #
+#    Updated: 2022/02/07 16:29:24 by lusokol          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 # The name of the binary
-NAME = Horde
+NAME = 
 
 # sources needed to compile
-SRC =	main.cpp \
-		ZombieHorde.cpp \
-		Zombie.cpp \
+SRC =	
 
 # path of sources
 PATH_SRCS = ./srcs
