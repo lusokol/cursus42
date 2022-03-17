455a13dd87c1873026af44f5dceebc1c0e6e1df6
Lucas SOKOL <lusokol@e1r8p16.42.fr>
2022-03-17 19:46:14 +0100

    cpp06 ex00 presque terminé

--- cercle5/cpp/CPP_06/ex00/includes/Litteral.hpp
+++ cercle5/cpp/CPP_06/ex00/includes/Litteral.hpp
@@ -6,7 +6,7 @@
 /*   By: lusokol <lusokol@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/17 13:37:41 by lusokol           #+#    #+#             */
-/*   Updated: 2022/03/17 14:57:56 by lusokol          ###   ########.fr       */
+/*   Updated: 2022/03/17 19:29:58 by lusokol          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,32 @@
 
 #include <iostream>
 
+#define INTMAX 2147483647
+#define INTMIN -2147483648
+#define CHAR 1
+#define INT 2
+#define DOUBLE 3
+#define FLOAT 4
+
 class Litteral {
 
 public:
 
-    Litteral(std::string toConvert);
+    Litteral(const char *toConvert);
     Litteral(Litteral const &ref);
     ~Litteral(void);
+    bool isInt(const char *str);
+    bool isDouble(const char *str);
+    bool isFloat(const char *str);
+    bool isChar(const char *str);
 
 private:
 
-    int type; // 1 = char, 2 = int, 3 = float, 4 = double
+    int type;
+    int _int;
+    char _char;
+    float _float;
+    double _double;
     std::string str;
 
 };
--- cercle5/cpp/CPP_06/ex00/srcs/Litteral.cpp
+++ cercle5/cpp/CPP_06/ex00/srcs/Litteral.cpp
@@ -6,15 +6,68 @@
 /*   By: lusokol <lusokol@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/17 14:51:49 by lusokol           #+#    #+#             */
-/*   Updated: 2022/03/17 14:59:38 by lusokol          ###   ########.fr       */
+/*   Updated: 2022/03/17 19:37:54 by lusokol          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Litteral.hpp"
 
-Litteral::Litteral(std::string toConvert) {
+bool Litteral::isInt(const char *toCheck){
+    char *end = NULL;
+	long int res = strtol(toCheck, &end, 10);
+
+	if (*end != '\0' || res > INTMAX || res < INTMIN)
+		return (false);
+
+	this->type = INT;
+	this->_int = static_cast<int>(res);
+	return (true);
+}
+
+bool Litteral::isDouble(const char *toCheck){
+    char *end = NULL;
+	long int res = strtod(toCheck, &end);
+
+	if (*end != '\0')
+		return (false);
+
+	this->type = DOUBLE;
+	this->_double = static_cast<double>(res);
+	return (true);
+}
+
+bool Litteral::isFloat(const char *toCheck){
+    char *end = NULL;
+	long int res = strtof(toCheck, &end);
+
+	if (*end != 'f' || *(end + 1) != '\0')
+		return (false);
+
+	this->type = FLOAT;
+	this->_float = static_cast<float>(res);
+	return (true);
+}
+
+bool Litteral::isChar(const char *toCheck){
+    char a = toCheck[0];
+
+	if ((toCheck[0] && toCheck[1] != '\0') || std::isprint(a) == false)
+		return (false);
+
+	this->type = CHAR;
+	this->_char = a;
+	return (true);
+}
+
+Litteral::Litteral(const char *toConvert) {
     std::cout << "Litteral constructor called" << std::endl;
-    (void)toConvert;
+    this->type = 0;
+    if (this->isInt(toConvert) == false)
+        if (this->isDouble(toConvert) == false)
+            if (this->isFloat(toConvert) == false)
+                this->isChar(toConvert) == false;
+    std::cout << "Arg type : " << this->type << std::endl;
+    this->aff_all();
 }
 
 Litteral::~Litteral(void) {
