b675eb7d2aab6284f08b073402255968a16b0e0d
Lucas SOKOL <lusokol@e1r12p20.42.fr>
2022-03-23 19:13:56 +0100

    ajout debut cpp_07

---
+++ cercle5/cpp/CPP_07/ex00/Makefile.settings.mk
@@ -0,0 +1,29 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    Makefile.settings.mk                               :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: lusokol <lusokol@student.42.fr>            +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2021/10/21 21:06:11 by rotrojan          #+#    #+#              #
+#    Updated: 2022/03/23 12:14:47 by lusokol          ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+# The name of the binary
+NAME = Functions
+
+# sources needed to compile
+SRC =	main.cpp \
+
+# path of sources
+PATH_SRCS = ./srcs
+
+# pathe of includes
+INCLUDES =	./includes/
+
+# flags for compilation
+CXXFLAGS = -Wall -Wextra -Werror -std=c++98# -g3 -fsanitize=address
+
+# compiler used
+CXX = c++
