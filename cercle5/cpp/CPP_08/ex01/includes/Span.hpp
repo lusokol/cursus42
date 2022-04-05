9851747cd4039cacc629d786602d8c808ee349e7
Lucas SOKOL <lusokol@e1r11p9.42.fr>
2022-04-05 17:38:34 +0200

    cpp08 ex01 terminé

--- cercle5/cpp/CPP_08/ex01/includes/Span.hpp
+++ cercle5/cpp/CPP_08/ex01/includes/Span.hpp
@@ -6,12 +6,14 @@
 /*   By: lusokol <lusokol@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/04 16:33:47 by lusokol           #+#    #+#             */
-/*   Updated: 2022/04/04 17:17:21 by lusokol          ###   ########.fr       */
+/*   Updated: 2022/04/05 17:10:02 by lusokol          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SPAN_CPP
 # define SPAN_CPP
+
+# include <vector>
 
 class Span {
     
@@ -23,21 +25,29 @@ class Span {
         Span &operator=(Span const &ref);
         void addNumber(int nbr);
         void affData(void);
+        template <typename T>
+        void addNumber(T it, T ite) {
+            if (this->data.size() + std::distance(it, ite) > this->size)
+                throw std::runtime_error("Cannot fill span from range : size too small");
+            for (; it != ite; it++)
+                this->data.push_back(*it);
+        }
         int longestSpan(void);
         int shortestSpan(void);
 
         int getSize(void) const {return this->size;}
-        int getIndex(void) const {return this->index;}
+        std::vector<int> getData(void) const {return this->data;}
 
     private:
 
-        int size;
-        int *data;
-        int index;
+        unsigned long int size;
+        std::vector<int> data;
 
         int findHigher(void);
         int findLower(void);
 
 };
+
+std::ostream &operator<<(std::ostream &ostream, Span const &ref);
 
 #endif
--- cercle5/cpp/CPP_08/ex01/srcs/main.cpp
+++ cercle5/cpp/CPP_08/ex01/srcs/main.cpp
@@ -6,28 +6,82 @@
 /*   By: lusokol <lusokol@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/04 17:05:24 by lusokol           #+#    #+#             */
-/*   Updated: 2022/04/04 17:15:41 by lusokol          ###   ########.fr       */
+/*   Updated: 2022/04/05 17:37:39 by lusokol          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "color.hpp"
 #include "Span.hpp"
+#include <iostream>
+#include <ctime>
+
+int myRand(void) {
+    static int seed __attribute__((unused)) = (srand(time(nullptr)), 0);
+    int res = rand() % 1000000;
+    return res;
+}
+
+void test(int size, int inside, int print = 0) {
+	Span test = Span(size);
+	std::vector<int> v(inside);
+	std::generate(v.begin(), v.end(), myRand);
+	static int check = 1;
+	try {
+		std::cout << YELLOW << "Test number " << check << std::endl;
+		check++;
+		std::cout << LGREEN << "Adding " << inside << " numbers in a span of size " << size << std::endl << STOP;
+		test.addNumber(v.begin(), v.end());
+		if (print == 0) {
+			std::cout << BLUE << "Data inside the new span :" << STOP << std::endl;
+			test.affData();
+		}
+	}
+	catch (std::exception &e) {
+		std::cout << RED << "Error: " << e.what() << std::endl << STOP;
+	}
+	try {
+		std::cout << BLUE << "Shortest span : " << LGREEN << test.shortestSpan() << STOP << std::endl;
+	}
+	catch (std::exception &e) {
+		std::cout << RED << "Error: " << e.what() << std::endl << STOP;
+	}
+	try {
+		std::cout << BLUE << "Longest span : " << LGREEN << test.longestSpan() << STOP << std::endl;
+	}
+	catch (std::exception &e) {
+		std::cout << RED << "Error: " << e.what() << std::endl << STOP;
+	}
+	std::cout << std::endl;
+}
 
 int main(void) {
-    std::cout << BLUE << "Creation of array step by step" << STOP << std::endl;
-    Span sp = Span(5);
-    sp.affData();
-    sp.addNumber(6);
-    sp.affData();
-    sp.addNumber(3);
-    sp.affData();
-    sp.addNumber(17);
-    sp.affData();
-    sp.addNumber(9);
-    sp.affData();
-    sp.addNumber(11);
-    sp.affData();
-
-    std::cout << BLUE << "Shortest span : " << LGREEN << sp.shortestSpan() << STOP << std::endl;
-    std::cout << BLUE << "Longest span : " << LGREEN << sp.longestSpan() << STOP << std::endl;
+	{
+    	std::cout << BLUE << "Creation of array step by step" << STOP << std::endl;
+    	Span sp = Span(5);
+    	sp.affData();
+    	sp.addNumber(6);
+    	sp.affData();
+    	sp.addNumber(3);
+    	sp.affData();
+    	sp.addNumber(17);
+    	sp.affData();
+    	sp.addNumber(9);
+    	sp.affData();
+    	sp.addNumber(11);
+    	sp.affData();
+    	try {sp.addNumber(1);}
+    	catch (std::exception &exce) {std::cout << RED << "Cannot add number : " << exce.what() << std::endl << STOP;}
+    	sp.affData();
+	
+	    std::cout << BLUE << "Shortest span : " << LGREEN << sp.shortestSpan() << STOP << std::endl;
+	    std::cout << BLUE << "Longest span : " << LGREEN << sp.longestSpan() << STOP << std::endl;
+		std::cout << std::endl;
+	}
+	test(6, 5);
+	test(5, 5);
+	test(1, 5);
+	test(1, 1);
+	test(0, 0);
+	test(10000000,10000000,1); // change 1 by 0 to print data
+	test(10000000,10000,1); //    change 1 by 0 to print data
 }
